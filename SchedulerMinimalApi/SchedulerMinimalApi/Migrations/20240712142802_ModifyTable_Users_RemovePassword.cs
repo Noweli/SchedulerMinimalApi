66883afcb1dced413d815a8553a03157fdd378ef
Patryk Lewon <patryklewonn@gmail.com>
2024-07-12 16:28:39 +0200

    Removed password from user

---
+++ SchedulerMinimalApi/SchedulerMinimalApi/Migrations/20240712142802_ModifyTable_Users_RemovePassword.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SchedulerMinimalApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModifyTable_Users_RemovePassword : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "Users");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "Users",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
